1a3895d3f8444f23d57c61ce3c921248b0f11258
andreeesz17 <andryzambrano1709@gmail.com>
2026-06-27 23:02:01 -0500

    configurar esquema de spectacular en settings

--- config/settings.py
+++ config/settings.py
@@ -21,6 +21,7 @@ INSTALLED_APPS = [
     'rest_framework_simplejwt.token_blacklist',
     'django_filters',
     'corsheaders',
+    'drf_spectacular',
     'motoshop',
 ]
 
@@ -80,6 +81,7 @@ REST_FRAMEWORK = {
     ],
     'DEFAULT_PAGINATION_CLASS': 'motoshop.pagination.StandardPagination',
     'PAGE_SIZE': 10,
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
 
 SIMPLE_JWT = {
