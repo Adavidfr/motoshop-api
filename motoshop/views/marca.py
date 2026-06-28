4143a57e7f8631235dbf0f2c2b59d61fc36e31e2
andreeesz17 <andryzambrano1709@gmail.com>
2026-06-27 22:52:56 -0500

    filtros de busqueda y ordenamiento en marcas

--- motoshop/views/marca.py
+++ motoshop/views/marca.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from motoshop.models.marca import Marca
 from motoshop.serializers.marca import MarcaSerializer
 from motoshop.permissions import IsAdminOrReadOnly
@@ -7,3 +9,6 @@ class MarcaViewSet(viewsets.ModelViewSet):
     queryset = Marca.objects.all()
     serializer_class = MarcaSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['nombre', 'descripcion']
+    ordering_fields = ['nombre', 'estado']
