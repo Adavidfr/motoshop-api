4f7e681ccb7107cc730d053bcee7c2492dafaf33
andreeesz17 <andryzambrano1709@gmail.com>
2026-06-27 22:53:17 -0500

    busqueda y ordenamiento en motos

--- motoshop/views/moto.py
+++ motoshop/views/moto.py
@@ -1,4 +1,6 @@
 from rest_framework import viewsets
+from rest_framework.filters import SearchFilter, OrderingFilter
+from django_filters.rest_framework import DjangoFilterBackend
 from motoshop.models.moto import Moto
 from motoshop.serializers.moto import MotoSerializer
 from motoshop.permissions import IsAdminOrReadOnly
@@ -7,3 +9,6 @@ class MotoViewSet(viewsets.ModelViewSet):
     queryset = Moto.objects.all()
     serializer_class = MotoSerializer
     permission_classes = [IsAdminOrReadOnly]
+    filter_backends = [DjangoFilterBackend, SearchFilter, OrderingFilter]
+    search_fields = ['modelo', 'color', 'marca__nombre', 'categoria__nombre']
+    ordering_fields = ['precio', 'anio', 'stock', 'modelo']
