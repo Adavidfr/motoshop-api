dcbb69fa8170d1a2ecf431824a390635e428e0b4
Karling23 <clalquinga@gmail.com>
2026-07-21 17:39:40 -0500

    Update pagination

--- motoshop/pagination.py
+++ motoshop/pagination.py
@@ -1,8 +1,8 @@
-from rest_framework.pagination import LimitOffsetPagination
+from rest_framework.pagination import PageNumberPagination
 
 
-class StandardPagination(LimitOffsetPagination):
-    default_limit      = 10
-    limit_query_param  = 'limit'
-    offset_query_param = 'offset'
-    max_limit          = 100
+class StandardPagination(PageNumberPagination):
+    page_size = 10
+    page_query_param = 'page'
+    page_size_query_param = 'limit'
+    max_page_size = 100
